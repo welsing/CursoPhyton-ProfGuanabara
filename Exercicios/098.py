c58c6bb3f9d1c5476e5e83c6c8b8f0625e6915f3
Florez <bernardowelsing90@gmail.com>
2023-06-17 20:03:24 -0300

    Exercicio 98

---
+++ Exercicios/098.py
@@ -0,0 +1,6 @@
+# 098 - FAça um programa que tenha uma função chamada contador(), que receba tres parametros:
+# Inicio, fim e passo e realize a contagem. Seu programa tem que realizar 3 contagens atraves da função criada.
+# a) de 1 até 10, de 1 em 1
+# b) de 10 até 0, de 2 em 2
+# c) Uma contagem personalizada
+
