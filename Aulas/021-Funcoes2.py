deffdda60b8d25b07ef5d572b9852266df223f64
Florezrj <bernardowelsing90@gmail.com>
2023-06-27 17:02:09 -0300

    Algumas anotações

---
+++ Aulas/021-Funcoes2.py
@@ -0,0 +1,6 @@
+# Help
+# Existe a função help() no phyton, que você pode ler a doc de uma função diretamente no console
+# ex: help(print)
+# Também podemos saber a doc desse metodo utilizando o  __doc__
+# ex: print(print.__doc__) faz a mesma coisa q help(print) só q com outra formatação
+
